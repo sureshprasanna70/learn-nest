04703fa7553e72fc9c32e55f3bf797a9785455b2
Suresh Prasanna <sureshprasanna70@gmail.com>
2020-04-20 12:43:42 +0530

    cats/cats.controller.ts: uses cats service and interface to perform get and post cats

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,25 @@
-import { Controller, Param, Get } from '@nestjs/common';
+import { Controller, Param, Get, Body, Post } from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { Cat } from './cats.interface'
+
 
 @Controller('cats')
 export class CatsController {
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-      return `This action returns a #${id} cat`;
-    }
+  constructor(private catsService: CatsService) { }
+  
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto)
+  }
+  
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} cat`;
+  }
 }
