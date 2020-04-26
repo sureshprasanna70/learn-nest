f54e32e0ee43a9630bbbf4f5bfd5c1fce7a6ead8
Suresh Prasanna <sureshprasanna70@gmail.com>
2020-04-26 12:39:30 +0530

    main.ts: prepare swagger documentation while bootstraping the application

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { configService } from './config/config.service';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  if (!configService.isProduction()) {
+
+    const document = SwaggerModule.createDocument(app, new DocumentBuilder()
+      .setTitle('Item API')
+      .setDescription('My Item API')
+      .build());
+
+    SwaggerModule.setup('docs', app, document);
+
+  }
   await app.listen(3000);
 
   if (module.hot) {
