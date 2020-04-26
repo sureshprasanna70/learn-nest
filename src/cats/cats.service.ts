dca9cbfb2c291dd79a639fbae6d8668e9f23dfab
Suresh Prasanna <sureshprasanna70@gmail.com>
2020-04-26 12:37:31 +0530

    cats/cats.service.ts: uses item dto to obfuscate database structure

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Item } from '../model/item.entity';
 import { Repository } from 'typeorm';
+import {ItemDTO} from './dto/item.dto'
 
 @Injectable()
 export class CatsService {
     constructor(@InjectRepository(Item) private readonly repo: Repository<Item>) {}
 
-    public async getAll() {
-        return await this.repo.find();
+    public async getAll(): Promise<ItemDTO[]> {
+        return await this.repo.find().then(items => items.map(e => ItemDTO.fromEntity(e)));
       }
 }
