cf575c8a8bba56ad4edff79aeb7ee347aca95516
Suresh Prasanna <sureshprasanna70@gmail.com>
2020-04-26 11:10:19 +0530

    src/cats/cats.module.ts: import item for cats module

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Item } from '../model/item.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Item])],
     controllers: [CatsController],
   providers: [CatsService],
   })
